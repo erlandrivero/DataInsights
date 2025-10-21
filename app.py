4c2ce7dd3b478de30fa1012462a957b642cf3d20
erlandrivero <your.email@example.com>
2025-10-21 18:58:14 -0400

    Fix Time Series error: Add missing plotly.graph_objects import

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from dotenv import load_dotenv
 from datetime import datetime
 import os
